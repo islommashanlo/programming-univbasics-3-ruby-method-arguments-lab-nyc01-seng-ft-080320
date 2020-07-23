25ae56fd6683075be951d64da740e0bbf195000f
Islom Mashanlo <islom.mashanlo@gmail.com>
2020-07-23 20:21:25 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -4,7 +4,7 @@
 # in order to complete the rest of this course and be ready for the next course!
 #introduction
 def introduction(name)
-  puts "Hi, my name is #{name}"
+  puts "Hi, my name is #{name}."
 end
 #introduction_with_language
 def introduction_with_language(name, language)
